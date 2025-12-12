bd72952f94d83e3550a74c576e11743fd365794c
Samet Selki <sametselki@outlook.com>
2025-12-13 02:17:31 +0300

    feat: add icon components and assets

---
+++ src/components/icons/IconChevronDown.jsx
@@ -0,0 +1,21 @@
+const IconChevronDown = ({ size = 24, color = 'currentColor' }) => {
+    return (
+        <svg
+            width={size}
+            height={size}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M6 9L12 15L18 9"
+                stroke={color}
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
+};
+
+export default IconChevronDown;
---
+++ src/components/icons/IconSearch.jsx
@@ -0,0 +1,27 @@
+const IconSearch = ({ size = 24, color = 'currentColor', ...props }) => (
+    <svg
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <path
+            d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
+            stroke={color}
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+        <path
+            d="M20.9999 21L16.6499 16.65"
+            stroke={color}
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+);
+
+export default IconSearch;
